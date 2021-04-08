c44873b3149ffc6567fa519add109213131c185b
Francisco Daines <francisco.daines@walmart.com>
2021-04-08 11:58:15 -0400

    Includes wrong dependencies

--- src/presentation/handler.go
+++ src/presentation/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/fdaines/arch-go-sample-project/src/businesslogic"
+	"github.com/fdaines/arch-go-sample-project/src/persistence"
 	"net/http"
 )
 
@@ -12,6 +13,9 @@ func CreateHandler() {
 }
 
 func handlerFunction(w http.ResponseWriter, r *http.Request){
+	dummyValue := persistence.RetrieveDataFromDatabase()
+	fmt.Printf("%s\n", dummyValue)
+
 	response := &Response{
 		Value: businesslogic.GetProcessedData(),
 	}
